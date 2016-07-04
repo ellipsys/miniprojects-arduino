6e3ad556f2fb2374defa792cf47257a17f1812f3
Eliseo Torres <eliseo.torres95@gmail.com>
2016-07-04 13:48:27 -0500

    Add files via upload

---
+++ ultrasonic.ino
@@ -0,0 +1,40 @@
+float distancia;
+float tiempo;
+int x;
+void setup(){
+  Serial.begin(9600);
+  pinMode(4, OUTPUT); /*activación del pin 9 como salida: para el pulso ultrasónico*/
+  pinMode(2, INPUT); /*activación del pin 8 como entrada: tiempo del rebote del ultrasonido*/
+  pinMode(10, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(4,LOW); /* Por cuestión de estabilización del sensor*/
+  delayMicroseconds(1);
+  digitalWrite(4, HIGH); /* envío del pulso ultrasónico*/
+  delayMicroseconds(10);
+  tiempo=pulseIn(2, HIGH); /* Función para medir la longitud del pulso entrante. Mide el tiempo que transcurrido entre el envío
+  del pulso ultrasónico y cuando el sensor recibe el rebote, es decir: desde que el pin 12 empieza a recibir el rebote, HIGH, hasta que
+  deja de hacerlo, LOW, la longitud del pulso entrante*/
+  distancia= float(0.017*tiempo); /*fórmula para calcular la distancia obteniendo un valor entero*/
+  /*Monitorización en centímetros por el monitor serial*/
+  x = int(distancia);
+  Serial.print("Distancia ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+  delay(500);
+  if (x>10.00){
+    digitalWrite(10, HIGH);
+    delay(500);
+    digitalWrite(10, LOW);
+    delay(500);
+    }
+   else{
+    digitalWrite(9, HIGH);
+    delay(500);
+    digitalWrite(9, LOW);
+    delay(500);
+    
+    }
+  
+}
